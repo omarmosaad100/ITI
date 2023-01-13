013b6a2774bb723974d9f88b858ae27d07bcd785
Omar Mosaad <omarmosaad2009@gmail.com>
2023-01-13 02:42:46 +0200

    new assignments

--- HTML5/day1/textcolor/textcolor.js
+++ HTML5/day1/textcolor/textcolor.js
@@ -5,3 +5,9 @@ blue = document.getElementById("blue");
 red.onchange = function() {
     document.getElementById("coloredText").style.color = "rgb("+red.value+","+green.value+","+blue.value+")";
 }
+green.onchange = function() {
+    document.getElementById("coloredText").style.color = "rgb("+red.value+","+green.value+","+blue.value+")";
+}
+blue.onchange = function() {
+    document.getElementById("coloredText").style.color = "rgb("+red.value+","+green.value+","+blue.value+")";
+}
